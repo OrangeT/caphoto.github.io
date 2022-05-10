d7c9c225ec1e044503ab06b9ca3b8e2647d1871f
Kian Ryan <kian@orangetentacle.co.uk>
2022-05-10 18:07:47 +0100

    Inital commit from WP

---
+++ wp-atom.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=httpwww.caphoto.co.uk/feed/atom/">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "httpwww.caphoto.co.uk/feed/atom/";
+		</script>
+
+		<p>You are being redirected to <a href="httpwww.caphoto.co.uk/feed/atom/">httpwww.caphoto.co.uk/feed/atom/</a></p>
+	</body>
+</html>
